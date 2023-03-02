6bc3c9e5e7e4df6c5690d79ce05a13b39d05006a
Thuan Nguyen <thuannguyen@Thuan-Nguyen.local>
2023-03-02 10:12:55 +0700

    Update Role demo

--- src/routes/Role.js
+++ src/routes/Role.js
@@ -1,11 +1,16 @@
+import "../styles/style.scss";
 import Button from "../components/Button";
 import Section from "../components/Section";
-import "../styles/style.scss";
-
+import Code from "../components/Code";
 import Intro from "../components/Intro";
 import Box from "../components/Box";
 
 export default function Role() {
+    const code = {
+        example1: `<button class="button">Button</button>`,
+        example2: `<span class="button">Button</span>`,
+        example3: ` <span role="button" class="button">Button</span>`,
+    };
     return (
         <>
             <Section>
@@ -14,16 +19,23 @@ export default function Role() {
             <Section>
                 <Box title="Native <button> element">
                     <Button tabIndex="1" />
+                    <Code code={code.example1} />
                 </Box>
+            </Section>
+            <Section>
                 <Box title="A <span> element">
                     <span tabIndex="1" className="button">
                         Button
                     </span>
+                    <Code code={code.example2} />
                 </Box>
+            </Section>
+            <Section>
                 <Box title="A <span> element with button role">
                     <span tabIndex="1" role="button" className="button">
                         Button
                     </span>
+                    <Code code={code.example3} />
                 </Box>
             </Section>
         </>
